6d37ceb4a75ee5c8299e151fe08c515c5aef656a
«Petrov <hiffchanel@mail.com>
2023-10-16 20:34:42 +0300

    feature: init-spring-maven-project

---
+++ src/main/java/com/vladhiff/emc/HelloController.java
@@ -0,0 +1,12 @@
+package com.vladhiff.emc;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Тестовый HTTP GET запрос. Хеллоу Ворлд!";
+    }
+}
